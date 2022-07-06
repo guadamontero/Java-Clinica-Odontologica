d190f00ac681a86d81900916258400a05f3c7e5f
Guadalupe Montero <guadamontero10@gmail.com>
2022-07-06 18:23:13 -0300

    .

--- src/main/java/com/dh/clinica/controller/PacienteController.java
+++ src/main/java/com/dh/clinica/controller/PacienteController.java
@@ -1,13 +1,10 @@
 package com.dh.clinica.controller;
 
-
-
 import com.dh.clinica.entities.Paciente;
 import com.dh.clinica.service.impl.PacienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
 
--- src/main/java/com/dh/clinica/entities/Domicilio.java
+++ src/main/java/com/dh/clinica/entities/Domicilio.java
@@ -1,3 +1,4 @@
+
 package com.dh.clinica.entities;
 
 import lombok.Getter;
